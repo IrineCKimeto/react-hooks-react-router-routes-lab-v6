5b337400058227b79358ae67a2175cc26add1376
IrineCKimeto <irenekimetoc@gmail.com>
2024-11-08 09:39:55 +0300

    adds src/components/MovieCard.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
+
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" activeClassName="active">Home</NavLink>
+      <NavLink to="/directors" activeClassName="active">Directors</NavLink>
+      <NavLink to="/actors" activeClassName="active">Actors</NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
+
